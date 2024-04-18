e73b1389595d24bc3379bfc861c7e344e7524492
Oumaima EL MARDI <82413301+oumaimaelmardi@users.noreply.github.com>
2024-04-18 14:46:16 +0000

    save method

--- src/pages/Documents.tsx
+++ src/pages/Documents.tsx
@@ -1,4 +1,17 @@
 import React, { useState, useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 import {
   Paper,
   Table,
@@ -8,7 +21,6 @@ import {
   TableHead,
   TableRow,
   TextField,
-  Button,
   Snackbar,
   Alert,
   Dialog,
@@ -23,8 +35,19 @@ import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import DocumentsDetails from "./DocumentsDetails";
 import AdvancedSearch from "./AdvancedSearch";
-
+import AddDocumentModal from "./AddDocumentModal";
+const pages = ["Documents", "User", "Acces"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 function Documents() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
   const [documents, setDocuments] = useState<Document[]>([]);
   const [selectedDocument, setSelectedDocument] = useState<Document | null>(
     null
@@ -86,7 +109,20 @@ function Documents() {
       .then((data) => setDocuments([data]))
       .catch((error) => console.error("Error fetching document by ID:", error));
   };
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   const handleSearch = () => {
     if (searchID) {
       loadDocumentById(searchID);
@@ -154,140 +190,314 @@ function Documents() {
       .catch((error) => console.error("Error downloading document:", error));
   };
 
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  const reloadDocuments = () => {
+    loadDocuments(searchID, searchNom, searchDate, searchType);
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        height: "100vh",
-      }}
-    >
-      <div style={{ marginBottom: "20px" }}>
-        <TextField
-          label="ID"
-          value={searchID}
-          onChange={(e) => setSearchID(e.target.value)}
-          style={{ marginRight: "10px" }}
-        />
-        <TextField
-          label="Nom"
-          value={searchNom}
-          onChange={(e) => setSearchNom(e.target.value)}
-          style={{ marginRight: "10px" }}
-        />
-        <TextField
-          label="Date de création"
-          type="date"
-          value={searchDate}
-          onChange={(e) => setSearchDate(e.target.value)}
-          InputLabelProps={{ shrink: true }}
-          style={{ marginRight: "10px" }}
-        />
-        <TextField
-          label="Type"
-          value={searchType}
-          onChange={(e) => setSearchType(e.target.value)}
-          style={{ marginRight: "10px" }}
-        />
-        <Button variant="contained" color="primary" onClick={handleSearch}>
-          Rechercher
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={handleSearchModalOpen}
-        >
-          Recherche avancée
-        </Button>
-      </div>
-      <TableContainer component={Paper} sx={{ maxWidth: 1000 }}>
-        <Table sx={{ maxWidth: 1000 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID du document</TableCell>
-              <TableCell align="right">Nom du document</TableCell>
-              <TableCell align="right">Type du document</TableCell>
-              <TableCell align="right">Date de création</TableCell>
-              <TableCell align="right">Détails</TableCell>
-              <TableCell align="right">Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {documents.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    <div>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Gestion des documents
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="center">{row.nom}</TableCell>
-                <TableCell align="center">{row.type}</TableCell>
-                <TableCell align="center">{row.dateCreation}</TableCell>
-                <TableCell align="center">
-                  <InfoIcon color="info" onClick={() => handleInfoClick(row)} />
-                </TableCell>
-                <TableCell align="center">
-                  <ArrowCircleDownIcon
-                    color="success"
-                    onClick={() => handleDownload(row.id)}
-                  />{" "}
-                  <DeleteForeverIcon
-                    color="error"
-                    onClick={() => handleDelete(row.id)}
-                  />{" "}
-                </TableCell>
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <div
+        style={{
+          width: "100%", // Adjust the width of the container
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            marginBottom: "20px",
+            marginTop: "50px",
+            width: "100%", // Make search fields take full width
+            display: "flex",
+            justifyContent: "center",
+            gap: "10px",
+          }}
+        >
+          <TextField
+            label="ID"
+            value={searchID}
+            onChange={(e) => setSearchID(e.target.value)}
+            // Adjust width of each TextField
+          />
+          <TextField
+            label="Nom"
+            value={searchNom}
+            onChange={(e) => setSearchNom(e.target.value)}
+            // Adjust width of each TextField
+          />
+          <TextField
+            label="Date de création"
+            type="date"
+            value={searchDate}
+            onChange={(e) => setSearchDate(e.target.value)}
+            InputLabelProps={{ shrink: true }}
+            // Adjust width of each TextField
+          />
+          <TextField
+            label="Type"
+            value={searchType}
+            onChange={(e) => setSearchType(e.target.value)}
+            // Adjust width of each TextField
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSearch}
+            // Adjust width of each Button
+          >
+            Rechercher
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleSearchModalOpen}
+          >
+            Recherche avancée
+          </Button>
+        </div>
+      </div>
+      <div
+        style={{
+          alignItems: "left",
+          marginLeft: "300px",
+          marginBottom: "70px",
+        }}
+      >
+        <Button variant="contained" onClick={handleOpenModal}>
+          Add Document
+        </Button>
+        <AddDocumentModal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          reloadDocuments={reloadDocuments}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <TableContainer component={Paper} sx={{ maxWidth: 1000 }}>
+          <Table sx={{ maxWidth: 1000 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID du document</TableCell>
+                <TableCell align="right">Nom du document</TableCell>
+                <TableCell align="right">Type du document</TableCell>
+                <TableCell align="right">Date de création</TableCell>
+                <TableCell align="right">Détails</TableCell>
+                <TableCell align="right">Actions</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {documents.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="center">{row.nom}</TableCell>
+                  <TableCell align="center">{row.type}</TableCell>
+                  <TableCell align="center">{row.dateCreation}</TableCell>
+                  <TableCell align="center">
+                    <InfoIcon
+                      color="info"
+                      onClick={() => handleInfoClick(row)}
+                    />
+                  </TableCell>
+                  <TableCell align="center">
+                    <ArrowCircleDownIcon
+                      color="success"
+                      onClick={() => handleDownload(row.id)}
+                    />{" "}
+                    <DeleteForeverIcon
+                      color="error"
+                      onClick={() => handleDelete(row.id)}
+                    />{" "}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
 
-      <AdvancedSearch
-        open={searchModalOpen}
-        handleClose={handleSearchModalClose}
-        handleSearchResults={handleSearchResults}
-      />
+        <AdvancedSearch
+          open={searchModalOpen}
+          handleClose={handleSearchModalClose}
+          handleSearchResults={handleSearchResults}
+        />
 
-      <DocumentsDetails
-        document={selectedDocument}
-        open={modalOpen}
-        handleClose={handleModalClose}
-      />
+        <DocumentsDetails
+          document={selectedDocument}
+          open={modalOpen}
+          handleClose={handleModalClose}
+        />
 
-      <Dialog
-        open={deleteDialogOpen}
-        onClose={cancelDelete}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Confirmation"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Are you sure you want to delete this document?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={cancelDelete} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={confirmDelete} color="primary" autoFocus>
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <Dialog
+          open={deleteDialogOpen}
+          onClose={cancelDelete}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Confirmation"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you sure you want to delete this document?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={cancelDelete} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={confirmDelete} color="primary" autoFocus>
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
 
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={6000}
-        onClose={() => setSnackbarOpen(false)}
-      >
-        <Alert severity="success" sx={{ width: "100%" }}>
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={() => setSnackbarOpen(false)}
+        >
+          <Alert severity="success" sx={{ width: "100%" }}>
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
+      </div>
     </div>
   );
 }
